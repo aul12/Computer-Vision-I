dce278dae632bfcd84f01a66c74e3c274d4a912b
Paul Nykiel <paul.nykiel@gmail.com>
2018-06-06 10:42:06 +0200

    Reworked exercise 2

--- Blatt03/sh03ex02.m
+++ Blatt03/sh03ex02.m
@@ -1,51 +1,59 @@
-load('filtered.mat');
+% Part 1
 load('Hfreq.mat');
 load('Hfreq2.mat');
-bookstore = imread('bookstore.tif');
-
-filtered_freq = fft2(filtered);
-bookstore_freq = fft2(bookstore);
 
 Hspat = ifft2(Hfreq);
 Hspat2 = ifft2(Hfreq2);
 
-subplot(5,2,1);
-imshow(filtered);
-title('Blurred Image');
+Hfreq_display = log(abs(fftshift(Hfreq))*12+1);
+Hfreq2_display = log(abs(fftshift(Hfreq2))*12+1);
 
-subplot(5,2,2);
-imshow(bookstore);
-title('Original Image');
 
-subplot(5,2,3);
-imshow(fftshift(filtered_freq));
-title('Fourier Transform of the Blurred Image');
+figure();
+subplot(2,2,1);
+imshow(Hfreq_display);
+title('Filter 1 in the frequency domain');
 
-subplot(5,2,4);
-imshow(fftshift(bookstore_freq));
-title('Fourier Transform of the Original Image');
-
-subplot(5,2,5);
-imshow(abs(fftshift(Hfreq)));
-title('Filter in the frequency domain');
-
-subplot(5,2,6);
-imshow(abs(fftshift(Hfreq2)));
+subplot(2,2,2);
+imshow(Hfreq2_display);
 title('Filter 2 in the frequency domain');
 
 spatialXRange = 1:30;
 spatialYRange = 1:50;
 
-subplot(5,2,7);
+subplot(2,2,3);
 imshow(Hspat(spatialXRange, spatialYRange));
 title('Filter 1 in the spatial domain');
 
-subplot(5,2,8);
+subplot(2,2,4);
 imshow(Hspat2(spatialXRange, spatialYRange));
 title('Filter 2 in the spatial domain');
 
-subplot(5,2,9);
-imshow(ifft2(filtered_freq./Hfreq));
-title('Deconvolved blurred image');
+print('sh03ex02_1.eps', '-depsc');
 
-print('sh03ex02.eps', '-depsc');
+% Part 2
+bookstore = imread('bookstore.tif');
+load('filtered.mat');
+
+filtered_freq = fft2(filtered);
+bookstore_freq = fft2(bookstore);
+
+figure();
+subplot(2,2,1);
+imshow(filtered);
+title('Blurred Image');
+
+subplot(2,2,2);
+imshow(bookstore);
+title('Original Image');
+
+subplot(2,2,3);
+imshow(ifft2(filtered_freq./Hfreq));
+
+title('Blurred Image Deconvolved with Filter 1');
+
+subplot(2,2,4);
+imshow(ifft2(filtered_freq./Hfreq2));
+title('Blurred Image Deconvolved with Filter 2');
+
+print('sh03ex02_2.eps', '-depsc');
